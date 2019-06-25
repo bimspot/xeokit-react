977238787dad5eca00e65499f486af8ef9b81491
Barnabás Molnár <barna.molnar90@gmail.com>
2019-06-25 15:50:54 +0200

    Refactored Screenshot demo to use refs.

--- demo/src/component-demos/Screenshot.js
+++ demo/src/component-demos/Screenshot.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { sampleModel1 } from "../models";
-import { GLTFViewer } from "xeokit-react";
+import React, { Component } from 'react';
+import { sampleModel1 } from '../models';
+import { GLTFViewer } from 'xeokit-react';
 
-const Screenshot = () => (
-    <GLTFViewer
-        canvasID="canvas-5"
-        width={600}
-        height={600}
-        models={[sampleModel1]}
-        enableScreenshot={true}
-    />
-);
+class Screenshot extends Component {
+  constructor() {
+    super();
+    this.viewerRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.viewerRef.current.takeScreenshot()}>
+          Take Screenshot
+        </button>
+        <GLTFViewer
+          canvasID="canvas-5"
+          width={600}
+          height={600}
+          models={[sampleModel1]}
+          enableScreenshot={false}
+          ref={this.viewerRef}
+        />
+      </div>
+    );
+  }
+}
 
 export default Screenshot;
