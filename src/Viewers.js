e95ca18bb6d092047a7de08807a18c971590cecf
Barnabás Molnár <barna.molnar90@gmail.com>
2019-08-08 15:02:56 +0200

    Fixed import order bug. Added a lint option with fix flag to package.json.

--- src/Viewers.js
+++ src/Viewers.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/order */
+import { makeViewer } from './Viewer';
 import { GLTFLoaderPlugin } from 'xeokit-sdk/src/plugins/GLTFLoaderPlugin/GLTFLoaderPlugin';
 import { XKTLoaderPlugin } from 'xeokit-sdk/src/plugins/XKTLoaderPlugin/XKTLoaderPlugin';
-import { makeViewer } from './Viewer';
 
 export const GLTFViewer = makeViewer(GLTFLoaderPlugin);
 export const XKTViewer = makeViewer(XKTLoaderPlugin);
